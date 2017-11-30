d273d7b3d914a3bb789b08afa4b8535ee7db33c0
VictorZhang <victor.zhang@thingtrust.com>
2017-11-30 09:38:08 +0800

    modi hub address

--- conf.js
+++ conf.js
@@ -9,7 +9,7 @@ exports.bLight = false;
 exports.storage = 'sqlite';
 
 
-exports.hub = 'trustnote.org/tn';
+exports.hub = 'hub2.trustnote.org/tn';
 exports.deviceName = 'Witness';
 exports.permanent_pairing_secret = 'randomstring';
 exports.control_addresses = ['DEVICE ALLOWED TO CHAT'];
